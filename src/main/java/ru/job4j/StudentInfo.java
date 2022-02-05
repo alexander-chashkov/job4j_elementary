816620a94f5b5942a12f7bc11725b0507c3566fb
Alexander Chashkov <ozzyzulka@gmail.com>
2022-02-05 18:57:38 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexander Chashkov");
+        System.out.println("25.11.1985");
+    }
+}
