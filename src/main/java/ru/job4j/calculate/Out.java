4689dbb7c618d4064557236bf730294ba133adfb
Alexander Chashkov <ozzyzulka@gmail.com>
2022-02-20 19:12:56 +0500

    9.1. String

---
+++ src/main/java/ru/job4j/calculate/Out.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+/**
+ * @author alex_chashkov
+ * @created 20/02/2022 - 19:09
+ * @project job4j
+ */
+public class Out {
+    public static void main(String[] args) {
+        String lyrics = "Мама мыла раму";
+        System.out.println(lyrics);
+        String name = "Petr";
+        String surname = "Arsentev";
+        String separate = " ";
+        String id = name + separate + surname;
+        System.out.println(id);
+    }
+}
---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+/**
+ * @author alex_chashkov
+ * @created 20/02/2022 - 19:10
+ * @project job4j
+ */
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + "But I am a newbie.";
+        int year = 2022;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
