d6efbd2830c8dcee7c97af07f5378b21740dc6f3
Alexander Chashkov <ozzyzulka@gmail.com>
2022-02-20 21:43:54 +0500

    1.1. Что такое import

---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,26 @@
+package ru.job4j.math;
+
+
+/**
+ * @author alex_chashkov
+ * @created 20/02/2022 - 21:34
+ * @project job4j
+ */
+public class MathFunction {
+
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+    public static double difference(double first, double second) {
+        return first - second;
+    }
+
+    public static double division (double first, double second) {
+        return first / second;
+    }
+}
