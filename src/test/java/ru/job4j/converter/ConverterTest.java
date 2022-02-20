a6910412a523721327b868d088728e106be3ab79
Alexander Chashkov <ozzyzulka@gmail.com>
2022-02-20 21:32:47 +0500

    1. Модульные тесты.

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author alex_chashkov
+ * @created 20/02/2022 - 20:29
+ * @project job4j
+ */
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert180RblThen3dot3333Dlr() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert200RblThen3dot3333Dlr() {
+        float in = 200;
+        float expected = 3.3333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
