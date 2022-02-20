5f2febcf186b7a0e920970a13e7c2fe4bdf75a8a
Alexander Chashkov <ozzyzulka@gmail.com>
2022-02-20 17:09:57 +0500

    5. Конвертер валюты

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,25 @@
+package ru.job4j.converter;
+
+/**
+ * @author alex_chashkov
+ * @created 20/02/2022 - 17:07
+ * @project job4j
+ */
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60; /* формула перевода рублей в доллары. */
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        float dol = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + dol + " dollar.");
+    }
+}
