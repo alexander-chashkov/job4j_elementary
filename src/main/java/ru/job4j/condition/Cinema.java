678f9a967c502746850c356284824111242426e4
Alexander Chashkov <ozzyzulka@gmail.com>
2022-03-21 21:37:30 +0500

    4.1.0. Оператор if с блоком else [#227534]

---
+++ src/main/java/ru/job4j/condition/Cinema.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+/**
+ * @author alex_chashkov
+ * @created 21/03/2022 - 21:35
+ * @project job4j
+ */
+public class Cinema {
+    public static void access(int age) {
+        System.out.println("The age of the customer is : " + age);
+        if (age >= 17) {
+            System.out.println("Welcome to the cinema.");
+        } else {
+            System.out.println("It is not for you.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Cinema.access(21);
+        Cinema.access(16);
+
+    }
+}
