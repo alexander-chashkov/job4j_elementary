b2d13ae90e3f9d3f9b94e8603baf404254a84cd1
Alexander Chashkov <ozzyzulka@gmail.com>
2022-03-24 22:12:24 +0500

    4.1.2. Глупый бот [#31323]

---
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author alex_chashkov
+ * @created 24/03/2022 - 22:09
+ * @project job4j
+ */
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        String in = "Привет, Бот.";
+        String result = DummyBot.answer(in);
+        String expected = "Привет, умник.";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String in = "Пока.";
+        String result = DummyBot.answer(in);
+        String expected = "До скорой встречи.";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String in = "Сколько будет 2 + 2?";
+        String result = DummyBot.answer(in);
+        String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
+        Assert.assertEquals(expected, result);
+    }
+}
