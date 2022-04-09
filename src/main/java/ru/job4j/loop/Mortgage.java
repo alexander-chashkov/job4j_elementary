94aa689888c135c58651f8b3e915ef151e78b61b
Alexander Chashkov <ozzyzulka@gmail.com>
2022-04-09 14:47:56 +0500

    5.7. Ипотека [#156315 #285839]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -7,12 +7,14 @@ package ru.job4j.loop;
  */
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        while (amount + amount * percent / 100 > salary) {
+        int year = 0;
+        double duty;
+        do  {
             year++;
             percent *= year;
             salary *= year;
-        }
+            duty = amount + amount * percent / 100;
+        } while (duty > salary);
         return year;
     }
 }
