b4d86f3e89777ef1cda433294687409d30af1135
Alexander Chashkov <ozzyzulka@gmail.com>
2022-04-09 14:27:36 +0500

    5.4. Протеиновая диета [#156313 #285818]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -7,9 +7,10 @@ package ru.job4j.loop;
  */
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        int month = ivan > nik ? 0 : 1;
-        while (ivan * 3 < nik * 2) {
+        int month = 0;
+        while (ivan <= nik) {
             ivan *= 3;
+            nik *= 2;
             month++;
         }
         return month;
