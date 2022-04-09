685395c8e4837df99e1ff08e0ea61cc984981df5
Alexander Chashkov <ozzyzulka@gmail.com>
2022-04-09 14:51:06 +0500

    5.9.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,14 @@
+package ru.job4j.live;
+
+/**
+ * @author alex_chashkov
+ * @created 09/04/2022 - 14:49
+ * @project job4j
+ */
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
