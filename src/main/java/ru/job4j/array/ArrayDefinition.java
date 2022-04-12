036e386533f1deac1fbc0b4dbbbf9ff52ac329b8
Alexander Chashkov <ozzyzulka@gmail.com>
2022-04-12 21:31:16 +0500

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+/**
+ * @author alex_chashkov
+ * @created 12/04/2022 - 21:25
+ * @project job4j
+ */
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
