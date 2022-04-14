47ba68798e09bdb31f18b82e21dd2ade9de99b6c
Alexander Chashkov <ozzyzulka@gmail.com>
2022-04-14 21:44:55 +0500

    6.3. Массив заполнен true или false [#53857]

---
+++ src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * @author alex_chashkov
+ * @created 14/04/2022 - 21:33
+ * @project job4j
+ */
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int i = 1; i < data.length - 1; i++) {
+            if (data[0] != data[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
