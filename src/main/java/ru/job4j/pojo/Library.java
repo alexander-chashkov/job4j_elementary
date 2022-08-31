1b407efe2fe57c24f4b7de176e47c13d8152568d
Alexander Chashkov <ozzyzulka@gmail.com>
2022-08-31 21:11:06 +0500

    3. Массивы и модели. [#174892 #337648]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -23,7 +23,7 @@ public class Library {
             System.out.println(books[i].getName() + "| страниц: " + books[i].getCountPage());
         }
         for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals(b.getName())) {
+            if ("Clean code".equals(books[i].getName())) {
                 System.out.println(books[i].getName() + "| страниц: " + books[i].getCountPage());
             }
         }
