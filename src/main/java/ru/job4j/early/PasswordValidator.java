60dafafc6bbc84531f0723b3eae5eabc641aff37
Alexander Chashkov <ozzyzulka@gmail.com>
2022-10-18 15:45:25 +0500

    2.1. Тест на ValidateInput

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PasswordValidator {
+    //fix
 
     public static String validate(String password) {
         if (password == null) {
